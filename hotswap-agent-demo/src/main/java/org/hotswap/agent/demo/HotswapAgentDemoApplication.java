dabf2dd8c018d17f36b3cc6177cecf564fd42d76
id-zxe <1078504296@qq.com>
2024-04-05 17:00:49 +0800

    添加spring boot demo,方便debug

---
+++ hotswap-agent-demo/src/main/java/org/hotswap/agent/demo/HotswapAgentDemoApplication.java
@@ -0,0 +1,13 @@
+package org.hotswap.agent.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotswapAgentDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotswapAgentDemoApplication.class, args);
+    }
+
+}
