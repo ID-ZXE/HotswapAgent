61fa4713fa4c8251f01d03bb528519ee96c7f105
id-zxe <1078504296@qq.com>
2024-04-15 08:59:29 +0800

    fix dubbo service autowire bug

--- plugin/hotswap-agent-dubbo-plugin/src/main/java/org/hotswap/agent/plugin/dubbo/transformers/DubboTransformers.java
+++ plugin/hotswap-agent-dubbo-plugin/src/main/java/org/hotswap/agent/plugin/dubbo/transformers/DubboTransformers.java
@@ -1,26 +1,9 @@
 package org.hotswap.agent.plugin.dubbo.transformers;
 
-import org.hotswap.agent.annotation.OnClassLoadEvent;
-import org.hotswap.agent.javassist.*;
 import org.hotswap.agent.logging.AgentLogger;
-import org.hotswap.agent.plugin.dubbo.DubboPlugin;
-import org.hotswap.agent.util.PluginManagerInvoker;
 
 public class DubboTransformers {
 
     private static final AgentLogger LOGGER = AgentLogger.getLogger(DubboTransformers.class);
-
-    // @OnClassLoadEvent(classNameRegexp = "org.apache.dubbo.config.spring.context.annotation.DubboClassPathBeanDefinitionScanner")
-    public static void patchDubboClassPathBeanDefinitionScanner(CtClass ctClass, ClassPool classPool) throws NotFoundException, CannotCompileException {
-        StringBuilder src = new StringBuilder("{");
-        src.append(PluginManagerInvoker.buildInitializePlugin(DubboPlugin.class));
-        // src.append("{if(true){return true;}}");
-        src.append("}");
-
-        CtMethod method = ctClass.getDeclaredMethod("checkCandidate");
-
-        method.insertBefore(src.toString());
-        LOGGER.info("patchDubboClassPathBeanDefinitionScanner success");
-    }
 
 }
